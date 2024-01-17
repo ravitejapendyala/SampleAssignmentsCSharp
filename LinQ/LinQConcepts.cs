26799e817c351cfea05325d014c4b35b484ea685
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-01-17 18:11:48 +0530

    Using linq to filter odd even numbers

--- LinQ/LinQConcepts.cs
+++ LinQ/LinQConcepts.cs
@@ -21,6 +21,21 @@ namespace SampleAssignments.LinQ
             var dist = listNumbers.Distinct().ToList();
             Console.WriteLine(dist[1]);
         }
+        [Test]
+        public void EvenOdd()
+        {
+            var numList = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            var even = numList.Where(x => x % 2 == 0).ToList();
+            var odd = numList.Where(x => x % 2 != 0).ToList();
+            even.ForEach(x => Console.WriteLine("Even are : "+x));
+            odd.ForEach(x => Console.WriteLine("odd are : " + x));            
+            Console.WriteLine("Even are : "+string.Join(", ", even));
+            Console.WriteLine("odd are : " + string.Join(", ", odd));
+            
+            
+            
+            
+        }
 
     }
 }
